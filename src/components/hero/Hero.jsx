9bafbf0c94a7b5e556bb6885f7ad8d92b1b7674d
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-02-17 20:12:14 +0500

    add

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './style.module.css';
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='getting'></div>
+            <div className='image'></div>
+        </div>
+    );
+};
+
+export default Hero;
