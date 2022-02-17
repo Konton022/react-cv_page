95efc9137cf30eed5f69536ebd905ab122a8038c
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-02-17 20:46:13 +0500

    add greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './pages/Main';
-import s from'./App.module.css'
 
 function App() {
     return (
-        <div className={s.App}>
+        <div className='App'>
             <Header />
             <Main />
             <Footer />
--- src/components/greeting/Greeting.jsx
+++ src/components/greeting/Greeting.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import * as cn  from 'classnames';
-import s from './style.module.css'
-import img from "./bird.jpg"
+import * as cn from 'classnames';
+import s from './style.module.css';
+import img from './bird.jpg';
 
 const Greeting = () => {
     return (
-    <>
-        <div className={s.greeting}>
-            <div className={cn(s.text, s.greeting__text)}>
-                <div className={s.text__title}>Hello World, </div>
-                <div className={s.text__discrib}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aperiam tempora, dolorum ipsam nemo nulla dolorem beatae
-                    voluptates dolore, odit ipsum asperiores aliquam?
-                    Consectetur minima, quasi molestiae perferendis aut cumque
-                    qui!
+        <>
+            <div className={s.greeting}>
+                <div className={cn(s.text, s.greeting__text)}>
+                    <div className={s.text__title}>
+                        Hello World, I'm Konstantin.{' '}
+                        <span>Junior React Front-End Developer</span>{' '}
+                    </div>
+                    <div className={s.text__discrib}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aperiam tempora, dolorum ipsam nemo nulla dolorem beatae
+                        voluptates dolore, odit ipsum asperiores aliquam?
+                        Consectetur minima, quasi molestiae perferendis aut
+                        cumque qui!
+                    </div>
+                    <a href='#'>Contact me</a>
                 </div>
-                <a href="#">Contact me</a>
+                <div className={s.greeting__image}>
+                    <img src={img} alt='bird' />
+                </div>
             </div>
-            <div className={s.greeting__image}><img src={img} alt="bird" /></div>
-        </div>
-    </>
+        </>
     );
 };
 
