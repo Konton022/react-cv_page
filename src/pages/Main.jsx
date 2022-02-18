fc1c1a837dce2d11fc1cccc0c55127bb440d9f4e
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-02-19 00:47:48 +0500

    added skills block

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Greeting from '../components/greeting/Greeting';
+import MySkills from '../components/myskills/MySkills';
 
 const Main = () => {
-	return (
-		<div>
-			<Greeting />
-		</div>
-	);
-}
+    return (
+        <div>
+            <Greeting />
+            <MySkills />
+        </div>
+    );
+};
 
 export default Main;
